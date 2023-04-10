c3e8091adbf167c772f1b8b123d4cab47c376304
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-04-10 11:23:18 -0400

    startup

---
+++ gestureApplication/gestureApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gestureApplication
+//
+//  Created by Canadore Student on 2023-04-10.
+//  Copyright © 2023 Canadore Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
