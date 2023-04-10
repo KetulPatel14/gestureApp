2e26422bb1939663f73b155908cf332443482ac3
KetulPatel14 <128503538+KetulPatel14@users.noreply.github.com>
2023-04-10 13:43:06 -0400

    Add

--- gestureApplication/gestureApplication/FourthViewController.swift
+++ gestureApplication/gestureApplication/FourthViewController.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class FourthViewController: UIViewController {
 
+    private let myView: UIView = {
+        let myView = UIView()
+        myView.backgroundColor = .systemPurple
+        return myView
+    }()
+    private let size: CGFloat = 200
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(myView)
+        myView.frame = CGRect(x: 0, y: 0, width: size, height: size)
+        myView.center = view.center
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didPinch(_ sender: UIPinchGestureRecognizer) {
+        if(sender.state == .changed){
+            let scale = sender.scale
+            myView.frame = CGRect(x: 0, y: 0, width: size, height: size)
+            myView.center = view.center
+            
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
